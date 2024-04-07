46310c50633290ff547b62adcfd14744a7eb78f3
Your local indian <a628472@outlook.com>
2024-04-07 14:15:59 -0500

    update

--- search.js
+++ search.js
@@ -1,17 +1,7 @@
-const gameTemplate = document.querySelector("[data-game-template]")
-
-
-fetch("games.json")
- .then(res => res.json())
- .then(data => {
-    data.forEach(game => {
-    const game = gameTemplate.content.cloneNode(true).children[0]
-    const rndrec = rndrec.querySelector("[data-rndrecdata]")
-
-    rndrecdata.textContent = game.name
-    rndrecdata.textContent = game.directory
-    append(game)
-
-    console.log(game)
-    })
- })
+function  SEARCH(){
+   var Search=document.getElementById("Search").value;
+   var result=window.find(Search);
+   if (result == false) {
+    alert("Nothing found or already highlighted "
+  ); }
+  } 
