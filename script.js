6ff010a6817f9281cb73901d10a2affccdc1a9fa
EclipsarGames <134024358+EclipsarGames@users.noreply.github.com>
2024-05-29 13:09:16 -0500

    simplified in attempt to fix freezing problem

--- script.js
+++ script.js
@@ -1,43 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', async () => {
   const searchInput = document.getElementById('searchInput');
   const searchResults = document.getElementById('searchResults');
   const searchResultTemplate = document.getElementById('searchResultTemplate').innerHTML;
 
   searchResults.innerHTML = 'Games are loading...';
-  
 
+  try {
+    const data = await fetch('games.json').then(response => response.json());
+    const renderSearchResults = (searchTerm = '') => {
+      searchResults.innerHTML = '';
+      const filteredResults = data.filter(item => item.title.toLowerCase().includes(searchTerm.toLowerCase()));
+      filteredResults.forEach(result => {
+        const renderedTemplate = searchResultTemplate.replace(/{{(.*?)}}/g, (match, key) => result[key.trim()]);
+        searchResults.innerHTML += renderedTemplate;
+      });
+    };
 
-  fetch('games.json')
-      .then(response => response.json())
-      .then(data => {
-          const mockData = data; 
+    renderSearchResults();
 
-          
-          function renderSearchResults(searchTerm = '') {
-              searchResults.innerHTML = '';
-
-              
-              const filteredResults = mockData.filter(item => item.title.toLowerCase().includes(searchTerm.toLowerCase()));
-
-              
-              filteredResults.forEach(result => {
-                  const renderedTemplate = renderTemplate(searchResultTemplate, result);
-                  searchResults.innerHTML += renderedTemplate;
-              });
-          }
-
-
-          renderSearchResults();
-
-          
-          searchInput.addEventListener('input', function() {
-              const searchTerm = searchInput.value.trim();
-              renderSearchResults(searchTerm);
-          });
-      })
-      .catch(error => console.error('Error fetching data:', error));
+    searchInput.addEventListener('input', () => {
+      const searchTerm = searchInput.value.trim();
+      renderSearchResults(searchTerm);
+    });
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
 });
-
-function renderTemplate(template, data) {
-  return template.replace(/{{(.*?)}}/g, (match, key) => data[key.trim()]);
-}
